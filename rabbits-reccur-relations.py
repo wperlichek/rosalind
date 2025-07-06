84d5143beb030c3b25d45f3989562d612ecf10d1
wperlichek <williamperlichek@gmail.com>
2025-07-06 12:38:06 -0700

    improve with DP

--- rabbits-reccur-relations.py
+++ rabbits-reccur-relations.py
@@ -1,17 +1,26 @@
+from typing import Dict
+
 MONTHS = 35
 LITTER_SIZE = 2
 
-# Crude, recursive, time complexity O(2^n)
-def rabbit_pairs(curr_month: int) -> int:
+# top down recursive with memoization
+def rabbit_pairs(curr_month: int, dp: Dict[int,int]) -> int:
     if curr_month <= 0:
         return 0
     elif curr_month == 1:
         return 1
+    elif curr_month in dp:
+        return dp[curr_month]
     else:
-        return rabbit_pairs(curr_month-1) + LITTER_SIZE * rabbit_pairs(curr_month-2)
+        res = rabbit_pairs(curr_month-1, dp) + LITTER_SIZE * rabbit_pairs(curr_month-2, dp)
+        dp[curr_month] = res
+        return dp[curr_month]
 
-pairs = rabbit_pairs(MONTHS)
+
+dp : Dict[int,int] = {}
+pairs = rabbit_pairs(MONTHS, dp)
 print(str(pairs))
+
 
 
 # Improve with DP to avoid duplicate calculations
